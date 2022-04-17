3849fc81674739da4f0bc05a7fdeaf2464aac6f1
JavGuerra <javguerra@badared.com>
2022-04-17 04:26:15 +0200

    cambios

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,9 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = 'lidao-pet-64.svg'
+
+html_favicon = 'favicon.ico'
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
